5880bbb027ec2bb2eb940390d9ae16979d8c3b11
Daniel Fox <danielfox@thoughtstoodefinite.com>
2020-10-05 11:27:45 -0400

    formatted code with black; revised README and moved to do list to tasks file

--- random_forest.py
+++ random_forest.py
@@ -1,15 +1,16 @@
 import sys
-sys.path.append('/Users/Daniel/Code/decision_tree')
-sys.path.append('/Users/Daniel/Code/random_forest')
+
+sys.path.append("/Users/Daniel/Code/decision_tree")
+sys.path.append("/Users/Daniel/Code/random_forest")
 import numpy as np
-np.random.seed(seed = 11)
+
+np.random.seed(seed=11)
 import importlib
 import pytest
 
 import decision_tree
+
 importlib.reload(decision_tree)
-
-
 
 
 def print_forest(forest):
@@ -17,14 +18,15 @@ def print_forest(forest):
         for node in tree:
             print(node)
 
-def forest_predict(forest, x, threshold = .5):
+
+def forest_predict(forest, x, threshold=0.5):
     """
     Predict the class from the random forest.
-    Input: 
+    Input:
     forest - a list of trees (see decision_tree.py doc for data structure of a tree)
     x - numpy array of features
     threshold - the threshold for predicting 1 or 0; default is 0.5
-    
+
     Output:
     class_predictions - 1D numpy array of integers (1 or 0)
     """
@@ -32,52 +34,60 @@ def forest_predict(forest, x, threshold = .5):
     for tree in forest:
         preds = decision_tree.predict(tree, x)
         predictions += preds
-    predictions = predictions/len(forest)
+    predictions = predictions / len(forest)
 
     class_predictions = (predictions >= threshold).astype(int)
     return class_predictions
 
-def grow_random_forest(X, y, num_trees = 10, 
-                       max_features = 'sqrt', 
-                       max_depth = 5, 
-                       min_node_size = 5,  
-                       max_samples = None,
-                       bootstrap = True,
-                       min_loss = .056,
-                       print_progress = False 
-                       ):
+
+def grow_random_forest(
+    X,
+    y,
+    num_trees=10,
+    max_features="sqrt",
+    max_depth=5,
+    min_node_size=5,
+    max_samples=None,
+    bootstrap=True,
+    min_loss=0.056,
+    print_progress=False,
+):
     """
     Create Random Forest Classifier
-    X, y - features, targets (np.arrays) 
-    num_trees - number of trees to grow, default = 10 
-      max_features - number of features randomly selected when splitting 
-      each node; default is 'sqrt' which uses the floor of the sqrt of 
-      the number of features 
-    max_depth - maximum allowed depth of any branch in the tree; default = 5, 
-    min_node_size - minimum allowed node size; default = 5,  
-    max_samples - the sample size to use - default = None 
+    X, y - features, targets (np.arrays)
+    num_trees - number of trees to grow, default = 10
+      max_features - number of features randomly selected when splitting
+      each node; default is 'sqrt' which uses the floor of the sqrt of
+      the number of features
+    max_depth - maximum allowed depth of any branch in the tree; default = 5,
+    min_node_size - minimum allowed node size; default = 5,
+    max_samples - the sample size to use - default = None
       results in using full data set,
     bootstrap - using bootstrapping results in using sampling with replacements; default = True
-    min_loss - if in growing the tree min_loss is exceeded or reached, 
+    min_loss - if in growing the tree min_loss is exceeded or reached,
       the growing process stops; default = .056 (1% accuracy for cross-entropy)
-    print_progress - print statement indicating that a tree was grown; default = False 
+    print_progress - print statement indicating that a tree was grown; default = False
     """
-    if max_features == 'sqrt':
+    if max_features == "sqrt":
         max_features = int(np.floor(np.sqrt(X.shape[1])))
     if max_samples is None:
         max_samples = X.shape[0]
     forest = []
     for i in range(num_trees):
-        sample_indices = np.random.choice(np.arange(X.shape[0]), size = max_samples, replace = bootstrap)
+        sample_indices = np.random.choice(
+            np.arange(X.shape[0]), size=max_samples, replace=bootstrap
+        )
         sample_features = X[sample_indices]
         sample_targets = y[sample_indices]
-        tree = decision_tree.grow_decision_tree(sample_features, sample_targets, max_depth, min_node_size, min_loss, max_features)
+        tree = decision_tree.grow_decision_tree(
+            sample_features,
+            sample_targets,
+            max_depth,
+            min_node_size,
+            min_loss,
+            max_features,
+        )
         forest.append(tree)
         if print_progress == True:
-            print(f'{i+1} trees have been grown.')
+            print(f"{i+1} trees have been grown.")
     return forest
-
-
-
-
-
--- tests_end2end/test_random_forest.py
+++ tests_end2end/test_random_forest.py
@@ -1,61 +1,113 @@
 import sys
-sys.path.append('/Users/Daniel/Code/decision_tree')
-sys.path.append('/Users/Daniel/Code/random_forest')
+
+sys.path.append("/Users/Daniel/Code/decision_tree")
+sys.path.append("/Users/Daniel/Code/random_forest")
 import numpy as np
-np.random.seed(seed = 11)
+
+np.random.seed(seed=11)
 import importlib
 import pytest
 
 import data_for_tests
+
 importlib.reload(data_for_tests)
 import plot_data
+
 importlib.reload(plot_data)
 import decision_tree
+
 importlib.reload(decision_tree)
 import random_forest
+
 importlib.reload(random_forest)
 import evaluation
+
 importlib.reload(evaluation)
 
-@pytest.mark.parametrize('num_points, classes, xbound, ybound, zbound, max_depth, min_node_size, min_loss, expected',
-	[
-	(np.full(8,1000), [0,0,0,0,0,0,0,1], .5, .5, .5, 30, 1, 0, [1, 1, 0]),
-	(np.full(8,1000), [1,0,0,0,0,0,0,1], .5, .5, .5, 30, 1, 0, [1, 1, 0])
-	])
-def test_octavo(num_points, classes, xbound, ybound, zbound, max_depth, min_node_size, min_loss, expected):
-	xy_parent = data_for_tests.make_octavo(num_points, classes, xbound, ybound, zbound).values
-	X = xy_parent[:,:-1]
-	y = xy_parent[:,-1]
 
-	forest = random_forest.grow_random_forest(X, y, num_trees = 20, max_features = 2, min_node_size = 1)
-	predictions = random_forest.forest_predict(forest, X)
-	targets = y
-	tfpns = evaluation.tfpn(predictions, targets)
-	cm = evaluation.make_confusion_matrix(*tfpns, percentage = True)
-	result = np.array([evaluation.precision(cm), evaluation.sensitivity(cm), evaluation.fpr(cm)])
-	expected = np.array(expected)
-	assert np.any(np.abs(expected - result) < 0.01)
+@pytest.mark.parametrize(
+    "num_points, classes, xbound, ybound, zbound, max_depth, min_node_size, min_loss, expected",
+    [
+        (
+            np.full(8, 1000),
+            [0, 0, 0, 0, 0, 0, 0, 1],
+            0.5,
+            0.5,
+            0.5,
+            30,
+            1,
+            0,
+            [1, 1, 0],
+        ),
+        (
+            np.full(8, 1000),
+            [1, 0, 0, 0, 0, 0, 0, 1],
+            0.5,
+            0.5,
+            0.5,
+            30,
+            1,
+            0,
+            [1, 1, 0],
+        ),
+    ],
+)
+def test_octavo(
+    num_points,
+    classes,
+    xbound,
+    ybound,
+    zbound,
+    max_depth,
+    min_node_size,
+    min_loss,
+    expected,
+):
+    xy_parent = data_for_tests.make_octavo(
+        num_points, classes, xbound, ybound, zbound
+    ).values
+    X = xy_parent[:, :-1]
+    y = xy_parent[:, -1]
 
-@pytest.mark.parametrize('num_points, dim, max_features, expected, precision_bound',
-	[
-	(1000, 2, 2, [1, 1, 0], .01),
-	(1000, 3, 2, [1, 1, 0], .01),
-	(1000, 4, 2, [1, 1, 0], .01)
-	# (10000, 10, 'sqrt', [1, 1, 0], .05), # commented out because they take a long time to run
-	# (10000, 20, 'sqrt', [1, 1, 0], .05) # commented out because they take a long time to run
-	])
+    forest = random_forest.grow_random_forest(
+        X, y, num_trees=20, max_features=2, min_node_size=1
+    )
+    predictions = random_forest.forest_predict(forest, X)
+    targets = y
+    tfpns = evaluation.tfpn(predictions, targets)
+    cm = evaluation.make_confusion_matrix(*tfpns, percentage=True)
+    result = np.array(
+        [evaluation.precision(cm), evaluation.sensitivity(cm), evaluation.fpr(cm)]
+    )
+    expected = np.array(expected)
+    assert np.any(np.abs(expected - result) < 0.01)
+
+
+@pytest.mark.parametrize(
+    "num_points, dim, max_features, expected, precision_bound",
+    [
+        (1000, 2, 2, [1, 1, 0], 0.01),
+        (1000, 3, 2, [1, 1, 0], 0.01),
+        (1000, 4, 2, [1, 1, 0], 0.01)
+        # (10000, 10, 'sqrt', [1, 1, 0], .05), # commented out because they take a long time to run
+        # (10000, 20, 'sqrt', [1, 1, 0], .05) # commented out because they take a long time to run
+    ],
+)
 def test_diagonal_ndim(num_points, dim, max_features, expected, precision_bound):
-	xy_parent = data_for_tests.make_diagonal_ndim(num_points, dim).values
-	X = xy_parent[:,:-1]
-	y = xy_parent[:,-1]
+    xy_parent = data_for_tests.make_diagonal_ndim(num_points, dim).values
+    X = xy_parent[:, :-1]
+    y = xy_parent[:, -1]
 
-	forest = random_forest.grow_random_forest(X, y, num_trees = 30, max_depth = 20, max_features = max_features, min_node_size = 1)
-	predictions = random_forest.forest_predict(forest, X)
-	targets = y
-	tfpns = evaluation.tfpn(predictions, targets)
-	cm = evaluation.make_confusion_matrix(*tfpns, percentage = True)
-	result = np.array([evaluation.precision(cm), evaluation.sensitivity(cm), evaluation.fpr(cm)])
-	expected = np.array(expected)
-	print(precision_bound)
-	assert np.any(np.abs(expected - result) < precision_bound)
-
+    forest = random_forest.grow_random_forest(
+        X, y, num_trees=30, max_depth=20, max_features=max_features, min_node_size=1
+    )
+    predictions = random_forest.forest_predict(forest, X)
+    targets = y
+    tfpns = evaluation.tfpn(predictions, targets)
+    cm = evaluation.make_confusion_matrix(*tfpns, percentage=True)
+    result = np.array(
+        [evaluation.precision(cm), evaluation.sensitivity(cm), evaluation.fpr(cm)]
+    )
+    expected = np.array(expected)
+    print(precision_bound)
+    assert np.any(np.abs(expected - result) < precision_bound)
