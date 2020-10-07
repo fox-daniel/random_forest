15205fd64c68f806672708a308456cd385d1a404
Daniel Fox <danielfox@thoughtstoodefinite.com>
2020-10-07 12:33:14 -0400

    removed conda env files; added rf_example.py

---
+++ rf_example.py
@@ -0,0 +1,41 @@
+import numpy as np
+import decision_tree
+import random_forest
+import evaluation
+import data_for_tests
+
+num_points, dim, max_features = 1000, 5, 2
+
+# generate data
+xy_parent = data_for_tests.make_diagonal_ndim(num_points, dim).values
+X = xy_parent[:, :-1]
+y = xy_parent[:, -1]
+
+# train the model -- grow the forest
+forest = random_forest.grow_random_forest(
+    X, y, num_trees=30, max_depth=20, max_features=max_features, min_node_size=1
+)
+
+# make predictions
+predictions = random_forest.forest_predict(forest, X)
+
+# calculate the numbers of true positives, false positives, true negatives, false negatives
+tfpns = evaluation.tfpn(predictions, y)
+
+# calculate the confusion matrix
+cm = evaluation.make_confusion_matrix(*tfpns, percentage=True)
+
+# calculate metrics: precision, sensitivity, false-positive-rate
+metrics = np.array(
+    [evaluation.precision(cm), evaluation.sensitivity(cm), evaluation.fpr(cm)]
+)
+
+print(
+    f"{num_points} are randomly generated in the unit cube in {dim}-dimensions.\n \
+Those with the sum of coordinates >= {dim}/2 are labeled 1, \n those below are \
+labeled 0."
+)
+print("The model achieves the following in sample metrics:")
+print("precision:", metrics[0])
+print("sensitivity:", metrics[1])
+print("false-positive-rate:", metrics[2])
