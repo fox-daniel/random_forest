01c738a9af21969c4d1ba86ccf85d86296ed479f
Daniel Fox <danielfox@thoughtstoodefinite.com>
2020-10-08 16:01:31 -0400

    added test script for random_forest that writes times; to use for testing numba

---
+++ random_forest_test.py
@@ -0,0 +1,101 @@
+import numpy as np
+import pandas as pd
+
+# import matplotlib.pyplot as plt
+# import seaborn as sns; sns.set()
+import importlib
+
+np.random.seed(seed=0)
+
+import decision_tree
+
+importlib.reload(decision_tree)
+
+import random_forest
+
+importlib.reload(random_forest)
+
+# from decision_tree import *
+from test_data import *
+
+import time
+from datetime import datetime
+
+report = open("random_forest_test_report", "a")
+report.write("***** New Run of Test *****\n")
+
+report.write(f"Time of run: {datetime.now()}\n")
+
+note = "Notes: \
+	 \n"
+
+if note:
+    report.write(note)
+
+# select type of test data
+testing_data = make_diagonal_ndim
+report.write(f"data type: {testing_data.__name__}\n")
+
+# select number of data points
+if testing_data == make_diagonal_ndim:
+    num_points = 100
+    dim = 5
+    report.write(f"{num_points} points in {dim} dimensions.\n")
+
+data = testing_data(num_points, dim)
+
+X = data.iloc[:, :-1].to_numpy()
+y = data.iloc[:, -1].to_numpy()
+
+# plot_data(data)
+
+# model parameters
+max_depth = 20
+min_node_size = 1
+report.write(f"max_depth = {max_depth}\n")
+report.write(f"min_node_size = {min_node_size}\n")
+
+# train model
+start = time.time()
+tree = random_forest.grow_random_forest(
+    X, y, 
+    num_trees=10,
+    max_features="sqrt",
+    max_depth=max_depth,
+    min_node_size=min_node_size,
+    max_samples=None,
+    bootstrap=True,
+    min_loss=0.056,
+    print_progress=False,
+)
+finish = time.time()
+duration = finish - start
+report.write(f"Growing the Random Forest took: {duration}\n")
+
+# report.write(f'Tree:\n')
+# for node in tree:
+# 	report.write('\n \n New Node: \n')
+# 	for cond in node[0]:
+# 		report.write(f'{cond}')
+# 		report.write('\n')
+
+# predict model
+start = time.time()
+preds = random_forest.forest_predict(tree, X)
+finish = time.time()
+duration = finish - start
+report.write(f"Predicting with the Random Forest took: {duration}\n")
+
+tfpns = tfpn(preds, data.Outcome)
+cm = make_confusion_matrix(*tfpns, percentage=True)
+# plot_cm(cm)
+report.write((f"Precision: {precision(cm)}, Recall: {sensitivity(cm)}\n"))
+
+report.write("***** End ****\n\n\n")
+
+
+# To profile a script and put the stats in a file:
+# python -m cProfile -o file_with_stats myscript.py
+
+# to visualize
+# snakeviz file_with_stats
